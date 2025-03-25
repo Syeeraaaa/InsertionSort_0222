1b6b275b9d73d4bd497493780aa13df219f9166a
Syeeraaaa <syeerasilvia@gmai.com>
2025-03-25 11:02:21 +0700

    membuat fungsi insertiosort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -8,6 +8,33 @@ void input() {
     while (true)
     {
         cout << "masukan jumlah Data pada Array : ";
+        cin>> n;
+
+        if (n <= 20 )
+        {
+            break;
+        }
+        else
+        {
+            cout<< "\nArray yang anda masukan maksimal 20 Elemen. \n";
+        }
     }
-    
+    cout<< endl;
+    cout<< "========================"<< endl;
+    cout<< "Masukkan Element Array"<< endl;
+    cout<< "========================"<< endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<< "Data Ke-" << (i + 1)<< ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionSort()
+{
+    int temp;
+    int j, i;
+
+    for (i = 1; i <= n - 1; i++)
 }
