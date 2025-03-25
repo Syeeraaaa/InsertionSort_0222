49d7092a60816f21dfe3c3bccd360c9397693dc3
Syeeraaaa <syeerasilvia@gmai.com>
2025-03-25 11:13:13 +0700

    menampilkan array yang terurut

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -37,4 +37,27 @@ void insertionSort()
     int j, i;
 
     for (i = 1; i <= n - 1; i++)
+    {
+        temp = arr[i];
+
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void display()
+{
+    cout<< endl;
+    cout<< "============================" << endl;
+    cout << "Total Pass =" << n - 1<< endl;
+    cout<< "============================" << endl;
+    cout<< "Element Array yang telah tersusun"<< endl;
+    cout<< "============================" << endl;
+
 }
